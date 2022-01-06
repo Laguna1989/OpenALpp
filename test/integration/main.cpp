25322232e6f75ede45f21ca8a87e0b43949a777e
Simon Weis <laguna.ws@gmail.com>
2022-01-06 18:45:29 +0100

    Set integration test sounds to a reasonable level

--- test/integration/main.cpp
+++ test/integration/main.cpp
@@ -9,12 +9,14 @@ int main()
 
     SoundData buf1 { "test1.ogg" };
     Sound snd1 { buf1, ctx };
+    snd1.setVolume(0.25f);
     snd1.play();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
     SoundData buf2 { "test.mp3" };
     Sound snd2 { buf2, ctx };
+    snd2.setVolume(0.25f);
     snd2.play();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(9000));
