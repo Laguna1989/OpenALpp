d856ce3cc665c108a61fae7fcca4c0ac304fc98a
Simon Weis <laguna_1989@gmx.net>
2022-01-11 17:32:17 +0100

    Fix linux build

--- impl/audio_exceptions.hpp
+++ impl/audio_exceptions.hpp
@@ -3,26 +3,33 @@
 
 #include <stdexcept>
 
-namespace oalpp
-{
-class AudioException : public std::exception
-{
+namespace oalpp {
+class AudioException : public std::exception {
 public:
     explicit AudioException(char const* const message)
-        : exception(message)
+        : m_message { message }
     {
     }
+
+    [[nodiscard]] char const* what() const override { return m_message; }
+
+private:
+    char const* m_message;
 };
 
-class AudioSystemException : public std::exception
-{
+class AudioSystemException : public std::exception {
 public:
     explicit AudioSystemException(char const* const message)
-        : exception(message)
+        : m_message { message }
     {
     }
+
+    [[nodiscard]] char const* what() const override { return m_message; }
+
+private:
+    char const* m_message;
 };
 
-}
+} // namespace oalpp
 
 #endif // OPENALPP_AUDIO_EXCEPTIONS_HPP
