3c255f4a7e0221c36efda617b046a4b5f40691ab
Simon Weis <laguna.ws@gmail.com>
2022-01-12 20:51:36 +0100

    Try defining float audio myself

--- impl/al.hpp
+++ impl/al.hpp
@@ -2,7 +2,13 @@
 #define OPENALPP_AL_HPP
 
 #include "AL/al.h"
-#ifndef __EMSCRIPTEN__
+#ifdef __EMSCRIPTEN__
+#ifndef AL_EXT_float32 // we assume AL_EXT_float32 is implemented
+#define AL_EXT_float32 1
+#define AL_FORMAT_MONO_FLOAT32 0x10010
+#define AL_FORMAT_STEREO_FLOAT32 0x10011
+#endif // AL_EXT_float32
+#else
 #include "AL/alext.h"
 #endif // __EMSCRIPTEN__
 
