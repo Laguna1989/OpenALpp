98d76002a3b8906867954c8d0bfba8f3fc097e3a
Simon Weis <laguna_1989@gmx.net>
2022-01-25 17:12:14 +0100

    Add EffectInterface and Butterworth

---
+++ impl/oalpp/effects/effect_interface.hpp
@@ -0,0 +1,14 @@
+#ifndef OPENALPP_EFFECTS_EFFECTINTERFACE_HPP
+#define OPENALPP_EFFECTS_EFFECTINTERFACE_HPP
+
+namespace oalpp {
+namespace effects {
+class MonoEffectInterface {
+public:
+    virtual ~MonoEffectInterface() = default;
+    virtual float process(float input) = 0;
+};
+} // namespace effects
+} // namespace oalpp
+
+#endif // OPENALPP_EFFECTINTERFACE_HPP
---
+++ impl/oalpp/effects/filter/filter_butter_worth_24_d_b.hpp
@@ -0,0 +1,29 @@
+#ifndef OPENALPP_EFFECTS_FILTER_BUTTERWORTH_24_D_B_HPP
+#define OPENALPP_EFFECTS_FILTER_BUTTERWORTH_24_D_B_HPP
+
+#include "oalpp/effects/effect_interface.hpp"
+
+namespace oalpp {
+namespace effects {
+namespace filter {
+
+class FilterButterworth24db : public oalpp::effects::MonoEffectInterface {
+public:
+    FilterButterworth24db();
+    void setSampleRate(float fs);
+    void set(float cutoff, float q);
+    float process(float input) override;
+
+private:
+    float t0, t1, t2, t3;
+    float coef0, coef1, coef2, coef3;
+    float history1, history2, history3, history4;
+    float gain;
+    float min_cutoff, max_cutoff;
+};
+
+} // namespace filter
+} // namespace effects
+} // namespace oalpp
+
+#endif // OPENALPP_EFFECTS_FILTER_BUTTERWORTH_24_D_B_HPP
