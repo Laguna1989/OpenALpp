13b0f415663a6b45eeb0b2aa5991544c428b59a7
Simon Weis <laguna.ws@gmail.com>
2022-02-16 18:23:09 +0100

    Add right to mono conversion

---
+++ impl/oalpp/sound_data/sound_data_right_to_mono.cpp
@@ -0,0 +1,25 @@
+
+#include "sound_data_right_to_mono.hpp"
+#include <stdexcept>
+
+namespace oalpp {
+
+SoundDataRightToMono::SoundDataRightToMono(SoundDataInterface& source)
+{
+    if (source.getNumberOfChannels() != 2) {
+        throw std::invalid_argument { "Can not convert left to mono from mono file." };
+    }
+
+    m_samples.resize(source.getSamples().size() / 2);
+
+    for (auto index = 0U; index != m_samples.size(); ++index) {
+        m_samples.at(index) = source.getSamples().at(index * 2 + 1);
+    }
+    m_sampleRate = source.getSampleRate();
+}
+
+int SoundDataRightToMono::getNumberOfChannels() const { return 1; }
+int SoundDataRightToMono::getSampleRate() const { return m_sampleRate; }
+std::vector<float> const& SoundDataRightToMono::getSamples() const { return m_samples; }
+
+} // namespace oalpp
--- test/unit_tests/sound_data_channel_conversion_test.cpp
+++ test/unit_tests/sound_data_channel_conversion_test.cpp
@@ -1,6 +1,7 @@
 #include "catch2/catch.hpp"
 #include "oalpp/sound_data/sound_data_left_to_mono.hpp"
 #include "oalpp/sound_data/sound_data_mono_to_stereo.hpp"
+#include "oalpp/sound_data/sound_data_right_to_mono.hpp"
 #include "sound_data_fake.hpp"
 
 using namespace oalpp;
@@ -58,6 +59,38 @@ TEST_CASE("SoundDataLeftToMono", "[SoundConversion, SoundDataLeftToMono]")
         fake.m_samples = std::vector<float> { 1.0f, 2.0f, 3.0f, 4.0f };
         std::vector<float> const expected_result { 1.0f, 3.0f };
         SoundDataLeftToMono mono { fake };
+
+        REQUIRE(expected_result == mono.getSamples());
+    }
+
+    SECTION("Raises exception on mono data")
+    {
+        SoundDataMonoFake fakeStereo {};
+        REQUIRE_THROWS(SoundDataLeftToMono { fakeStereo });
+    }
+}
+
+TEST_CASE("SoundDataRightToMono", "[SoundConversion, SoundDataRightToMono]")
+{
+    SoundDataStereoFake fake {};
+
+    SECTION("Correct number of channels")
+    {
+        SoundDataRightToMono mono { fake };
+        REQUIRE(mono.getNumberOfChannels() == 1);
+    }
+
+    SECTION("Same sample rate as original")
+    {
+        SoundDataRightToMono mono { fake };
+        REQUIRE(mono.getSampleRate() == fake.getSampleRate());
+    }
+
+    SECTION("Correct samples after conversion")
+    {
+        fake.m_samples = std::vector<float> { 1.0f, 2.0f, 3.0f, 4.0f };
+        std::vector<float> const expected_result { 2.0f, 4.0f };
+        SoundDataRightToMono mono { fake };
 
         REQUIRE(expected_result == mono.getSamples());
     }
