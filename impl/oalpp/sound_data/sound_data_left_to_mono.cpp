83dc0127f896d8faa332db3784272a24993bc5ed
Simon Weis <laguna.ws@gmail.com>
2022-02-16 18:20:14 +0100

    Add tests and implementation for left to mono conversion

---
+++ impl/oalpp/sound_data/sound_data_left_to_mono.cpp
@@ -0,0 +1,25 @@
+
+#include "sound_data_left_to_mono.hpp"
+#include <stdexcept>
+
+namespace oalpp {
+
+SoundDataLeftToMono::SoundDataLeftToMono(SoundDataInterface& source)
+{
+    if (source.getNumberOfChannels() != 2) {
+        throw std::invalid_argument { "Can not convert left to mono from mono file." };
+    }
+
+    m_samples.resize(source.getSamples().size() / 2);
+
+    for (auto index = 0U; index != m_samples.size(); ++index) {
+        m_samples.at(index) = source.getSamples().at(index * 2);
+    }
+    m_sampleRate = source.getSampleRate();
+}
+
+int SoundDataLeftToMono::getNumberOfChannels() const { return 1; }
+int SoundDataLeftToMono::getSampleRate() const { return m_sampleRate; }
+std::vector<float> const& SoundDataLeftToMono::getSamples() const { return m_samples; }
+
+} // namespace oalpp
--- test/unit_tests/sound_data_channel_conversion_test.cpp
+++ test/unit_tests/sound_data_channel_conversion_test.cpp
@@ -1,4 +1,5 @@
 #include "catch2/catch.hpp"
+#include "oalpp/sound_data/sound_data_left_to_mono.hpp"
 #include "oalpp/sound_data/sound_data_mono_to_stereo.hpp"
 #include "sound_data_fake.hpp"
 
@@ -33,5 +34,37 @@ TEST_CASE("SoundDataMonoToStereo", "[SoundConversion, SoundDataMonoToStereo]")
     {
         SoundDataStereoFake fakeStereo {};
         REQUIRE_THROWS(SoundDataMonoToStereo { fakeStereo });
+    }
+}
+
+TEST_CASE("SoundDataLeftToMono", "[SoundConversion, SoundDataLeftToMono]")
+{
+    SoundDataStereoFake fake {};
+
+    SECTION("Correct number of channels")
+    {
+        SoundDataLeftToMono mono { fake };
+        REQUIRE(mono.getNumberOfChannels() == 1);
+    }
+
+    SECTION("Same sample rate as original")
+    {
+        SoundDataLeftToMono mono { fake };
+        REQUIRE(mono.getSampleRate() == fake.getSampleRate());
+    }
+
+    SECTION("Correct samples after conversion")
+    {
+        fake.m_samples = std::vector<float> { 1.0f, 2.0f, 3.0f, 4.0f };
+        std::vector<float> const expected_result { 1.0f, 3.0f };
+        SoundDataLeftToMono mono { fake };
+
+        REQUIRE(expected_result == mono.getSamples());
+    }
+
+    SECTION("Raises exception on mono data")
+    {
+        SoundDataMonoFake fakeStereo {};
+        REQUIRE_THROWS(SoundDataLeftToMono { fakeStereo });
     }
 }
