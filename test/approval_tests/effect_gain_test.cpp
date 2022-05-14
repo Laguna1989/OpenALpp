017a7e2fa2f98a74fc1210494b1906aa1295ff2a
Simon Weis <laguna_1989@gmx.net>
2022-05-14 16:00:30 +0200

    Add first two approval tests for gain effect

---
+++ test/approval_tests/effect_gain_test.cpp
@@ -0,0 +1,26 @@
+#include "ApprovalTests/ApprovalTests.hpp"
+#include "catch2/catch.hpp"
+#include "oalpp/effects/utility/gain.hpp"
+#include "oalpp/sound_data.hpp"
+
+TEST_CASE("effect gain")
+{
+    SECTION("1.0")
+    {
+        std::string const fileName { "assets/test1.ogg" };
+        oalpp::effects::utility::Gain gain { 1.0f };
+        oalpp::SoundData buffer { fileName };
+        oalpp::SoundDataWithEffect soundWithEffect { buffer, gain };
+
+        ApprovalTests::Approvals::verifyAll(soundWithEffect.getSamples());
+    }
+    SECTION("2.0")
+    {
+        std::string const fileName { "assets/test1.ogg" };
+        oalpp::effects::utility::Gain gain { 2.0f };
+        oalpp::SoundData buffer { fileName };
+        oalpp::SoundDataWithEffect soundWithEffect { buffer, gain };
+
+        ApprovalTests::Approvals::verifyAll(soundWithEffect.getSamples());
+    }
+}
